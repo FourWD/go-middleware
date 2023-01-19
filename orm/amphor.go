00ae51d2ec5821bbcd0f3bcae236b66b3b74326b
Pattanapong <pattanapong.amu@gmail.com>
2023-01-19 14:34:07 +0700

    test

--- orm/amphor.go
+++ orm/amphor.go
@@ -5,6 +5,6 @@ type Amphor struct {
 	GormModel
 
 	Name       string ` db:"name" json:"name" gorm:"type:varchar(150);" `
-	NameEn     string ` db:"name_en" json:"name_en" gorm:"type:varchar(150);" `
+	NameEn     string ` db:"name_en" json:"name_en" gorm:"type:varchar(100);" `
 	ProvinceID string ` db:"province_id" json:"province_id" gorm:"type:varchar(36);index;" `
 }
