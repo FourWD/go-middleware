602247050bb10f4e0f5a5edfe7e78bc9bc04b733
Pattanapong <pattanapong.amu@gmail.com>
2023-02-04 04:31:54 +0700

    update orm change to id
    update spec

--- orm/vehicle.go
+++ orm/vehicle.go
@@ -10,23 +10,17 @@ type Vehicle struct {
 
 	DomainCode string `db:"domain_code"  json:"domain_code" gorm:"type:varchar(50); index"`
 
-	Code              string `db:"code"  json:"code" gorm:"type:varchar(50) ; dafault:null ; index"`
-	Source            string `db:"source"  json:"source" gorm:"type:varchar(50) ; dafault:null "`
-	Grade             string `db:"grade" json:"grade" gorm:"type:varchar(50) ; dafault:null "`
-	YearBu            string `db:"year_bu" json:"year_bu" gorm:"type:varchar(4) ; dafault:null "`
-	YearReg           string `db:"year_reg" json:"year_reg" gorm:"type:varchar(4) ; dafault:null "`
-	VehicleTypeID     string `db:"vehicle_type_id" json:"vehicle_type_id" gorm:"type:varchar(36);"`
-	VehicleBrandID    string `db:"vehicle_brand_id" json:"vehicle_brand_id" gorm:"type:varchar(36) "`
-	VehicleModelID    string `db:"vehicle_model_id" json:"vehicle_model_id" gorm:"type:varchar(36);"`
-	VehicleSubmodelID string `db:"vehicle_submodel_id" json:"vehicle_submodel_id" gorm:"type:varchar(36); "`
-	BranchID          string `db:"branch_id" json:"branch_id" gorm:"type:varchar(36); "`
-	ColorID           string `db:"color_id" json:"color_id" gorm:"type:varchar(36);"`
-	// DriveSystem       string    `db:"drive_system" json:"drive_system" gorm:"type:varchar(50) ; dafault:null "`
-	// GearTypeID        string    `db:"gear_type_id" json:"gear_type_id" gorm:"type:varchar(36);  "`
-	// EngineCapacity    string    `db:"engine_capacity" json:"engine_capacity" gorm:"type:varchar(50) ; dafault:null "`
-	// EngineOilID       string    `db:"engine_oil_id" json:"engine_oil_id" gorm:"type:varchar(36)"`
-	// EngineSize        string    `db:"engine_size" json:"engine_size" gorm:"type:varchar(50) ; dafault:null "`
-	// Seat              int       `db:"seat" json:"seat" gorm:"type:int(11) ; dafault:null "`
+	Code              string    `db:"code"  json:"code" gorm:"type:varchar(50) ; dafault:null ; index"`
+	Source            string    `db:"source"  json:"source" gorm:"type:varchar(50) ; dafault:null "`
+	Grade             string    `db:"grade" json:"grade" gorm:"type:varchar(50) ; dafault:null "`
+	YearBu            string    `db:"year_bu" json:"year_bu" gorm:"type:varchar(4) ; dafault:null "`
+	YearReg           string    `db:"year_reg" json:"year_reg" gorm:"type:varchar(4) ; dafault:null "`
+	VehicleTypeID     string    `db:"vehicle_type_id" json:"vehicle_type_id" gorm:"type:varchar(36);"`
+	VehicleBrandID    string    `db:"vehicle_brand_id" json:"vehicle_brand_id" gorm:"type:varchar(36) "`
+	VehicleModelID    string    `db:"vehicle_model_id" json:"vehicle_model_id" gorm:"type:varchar(36);"`
+	VehicleSubmodelID string    `db:"vehicle_submodel_id" json:"vehicle_submodel_id" gorm:"type:varchar(36); "`
+	BranchID          string    `db:"branch_id" json:"branch_id" gorm:"type:varchar(36); "`
+	ColorID           string    `db:"color_id" json:"color_id" gorm:"type:varchar(36);"`
 	Mileage           int       `db:"mileage" json:"mileage" gorm:"type:int(11) "`
 	LicensePlate      string    `db:"license_plate" json:"license_plate" gorm:"type:varchar(50) ; dafault:null "`
 	LicenseProvinceID string    `db:"license_province_id" json:"license_province_id" gorm:"type:varchar(36) "`
