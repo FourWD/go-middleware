9c1532e4eae33468b0d54c97762d5e75feeb9e38
Pattanapong <pattanapong.amu@gmail.com>
2023-02-07 11:30:32 +0700

    create new
    update

--- orm/gear-type.go
+++ orm/gear-type.go
@@ -4,5 +4,5 @@ type GearType struct {
 	ID string `db:"id" json:"id" gorm:"type:varchar(36);primary_key;"`
 	GormModel
 
-	Name string ` db:"name" json:"name" gorm:"type:varchar(50);index " `
+	Name string ` db:"name" json:"name" gorm:"type:varchar(200);" `
 }
--- orm/vehicle-image.go
+++ orm/vehicle-image.go
@@ -8,5 +8,6 @@ type VehicleImage struct {
 	VehicleID           string `db:"vehicle_id"  json:"vehicle_id" gorm:"not null; type:varchar(50)"`
 	ImagePath           string `db:"image_path"  json:"image_path" gorm:"dafault:null; type:varchar(100)"`
 	Description         string `db:"description"  json:"description" gorm:"dafault:null; type:varchar(100)"`
+	IsMain              bool   `db:"is_main"  json:"is_main" gorm:"type:tinyint(1)"`
 	RowOrder            string `db:"row_order"  json:"row_order" gorm:"type:varchar(100)"`
 }
