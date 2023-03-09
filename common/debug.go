ce6e84d32fe7f9af121b812babd74292a73c7226
Pattanapong <pattanapong.amu@gmail.com>
2023-03-09 12:42:55 +0700

    update debug
    terminal print error update

--- common/debug.go
+++ common/debug.go
@@ -5,14 +5,24 @@ import (
 	"time"
 )
 
-func Print(label string, value string) {
-	colorReset := "\033[0m"
-	colorGreen := "\033[32m"
+const colorReset string = "\033[0m"
+const colorGreen string = "\033[32m"
+const colorRed string = "\033[31m"
 
+func Print(label string, value string) {
 	fmt.Println(string(colorReset), ``)
 	fmt.Println(string(colorGreen), `=========================================`)
-	fmt.Println(string(colorGreen), time.Now().Format("YYYY-MM-DD hh:mm:ss")+" : "+label)
+	fmt.Println(string(colorGreen), time.Now().Format("2017-09-07 17:06:06")+" : "+label)
 	fmt.Println(string(colorGreen), value)
 	fmt.Println(string(colorGreen), `=========================================`)
+	fmt.Println(string(colorReset), ``)
+}
+
+func PrintError(label string, value string) {
+	fmt.Println(string(colorReset), ``)
+	fmt.Println(string(colorRed), `=========================================`)
+	fmt.Println(string(colorRed), time.Now().Format("2017-09-07 17:06:06")+" : "+label)
+	fmt.Println(string(colorRed), value)
+	fmt.Println(string(colorRed), `=========================================`)
 	fmt.Println(string(colorReset), ``)
 }
